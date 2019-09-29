a2ab8c903e09494f3a69ea5420e49fd0de330001
aom <aom@interlogic.com.ua>
2019-09-29 17:34:29 +0300

    Added elementary UI Added map

---
+++ UniversityProgramm/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Xamarin.Forms.Platform.WPF;
+
+namespace UniversityProgramm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Expander_Button_click(object sender, RoutedEventArgs e)
+        {
+            if (LeftExpandPanel.Visibility == Visibility.Collapsed)
+            {
+                LeftExpandPanel.Visibility = Visibility.Visible;
+                //((Button)sender).Margin = new Thickness(134, 0, 0, 0);
+                ((Button)sender).Content = "-->";
+            }
+            else
+            {
+                LeftExpandPanel.Visibility = Visibility.Collapsed;
+                //((Button)sender).Margin = new Thickness(0, 0, 0, 0);
+                //((Button)sender).Margin = new Thickness(134, 0, 0, 0);
+                ((Button)sender).Content = "<--";
+            }
+        }
+    }
+}
