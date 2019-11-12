29c8cc44eba8690781b87ef901725cb3fef44319
Osttik <lololka23@gmail.com>
2019-11-12 23:34:00 +0200

    Added new image Changed UI

--- UniversityProgramm/MainWindow.xaml.cs
+++ UniversityProgramm/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
-﻿using Microsoft.Maps.MapControl.WPF;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Xamarin.Forms.Platform.WPF;
 
 namespace UniversityProgramm
 {
@@ -21,26 +11,48 @@ namespace UniversityProgramm
     /// </summary>
     public partial class MainWindow : Window
     {
+        public double MaximumHeight { get => Height; } 
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Expander_Button_click(object sender, RoutedEventArgs e)
+        //private void Expander_Button_click(object sender, RoutedEventArgs e)
+        //{
+        //    if (LeftExpandPanel.Visibility == Visibility.Collapsed)
+        //    {
+        //        LeftExpandPanel.Visibility = Visibility.Visible;
+        //        ((Button)sender).Content = "-->";
+        //    }
+        //    else
+        //    {
+        //        LeftExpandPanel.Visibility = Visibility.Collapsed;
+        //        ((Button)sender).Content = "<--";
+        //    }
+        //}
+
+        private void Exit(object sender, RoutedEventArgs e)
         {
-            if (LeftExpandPanel.Visibility == Visibility.Collapsed)
-            {
-                LeftExpandPanel.Visibility = Visibility.Visible;
-                //((Button)sender).Margin = new Thickness(134, 0, 0, 0);
-                ((Button)sender).Content = "-->";
-            }
-            else
-            {
-                LeftExpandPanel.Visibility = Visibility.Collapsed;
-                //((Button)sender).Margin = new Thickness(0, 0, 0, 0);
-                //((Button)sender).Margin = new Thickness(134, 0, 0, 0);
-                ((Button)sender).Content = "<--";
-            }
+            Environment.Exit(0);
+        }
+
+        public void DrawLine(Point firstPoint, Point secondPoint)
+        {
+            Line line = new Line();
+            line.X1 = firstPoint.X;
+            line.X2 = secondPoint.X;
+            line.Y1 = firstPoint.Y;
+            line.Y2 = secondPoint.Y;
+
+            line.Stroke = Brushes.Blue;
+            line.StrokeThickness = 2;
+            MainGrid.Children.Add(line);
+        }
+
+        private void Find(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
